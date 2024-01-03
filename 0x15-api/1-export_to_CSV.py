ca7a638c691d81faec71388d3630e4f3996c1311
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-01-03 09:34:56 -0800

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,39 +1,32 @@
 #!/usr/bin/python3
-"""Retrieve and export TODO list information
-for a given employee ID in CSV format.
-"""
-import requests
+"""Export TODO list information for a
+given employee ID to CSV format."""
 import csv
+import requests
 import sys
 
 if __name__ == "__main__":
+    employee_id = sys.argv[1]
+
     api_url = "https://jsonplaceholder.typicode.com/"
 
-    user_response = requests.get(api_url + "users/{}".format(sys.argv[1]))
+    user_response = requests.get(api_url + "users/{}".format(employee_id))
     user_data = user_response.json()
 
+    username = user_data.get("username")
+
     todos_response = requests.get(api_url + "todos",
-                                  params={"userId": sys.argv[1]})
+                                  params={"userId": employee_id})
     todos_data = todos_response.json()
 
-    csv_file_path = "{}.csv".format(sys.argv[1])
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-    with open(csv_file_path, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["USER_ID", "USERNAME",
+                             "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-        csv_writer.writerow([
-            "USER_ID",
-            "USERNAME",
-            "TASK_COMPLETED_STATUS",
-            "TASK_TITLE"
-            ])
+        [csv_writer.writerow(
+            [employee_id, username, task.get("completed"), task.get("title")]
+         ) for task in todos_data]
 
-        for task in todos_data:
-            csv_writer.writerow([
-                sys.argv[1],
-                user_data.get("username"),
-                str(task.get("completed")),
-                task.get("title")
-            ])
-
-    print("Data exported to {}".format(csv_file_path))
+        print("Data exported to {}.csv".format(employee_id))
