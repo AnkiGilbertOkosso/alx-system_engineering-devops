e30155f081508803abe110a2dc71fd296a81881b
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-01-18 09:01:34 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,15 @@ def number_of_subscribers(subreddit):
     request_headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /i/bdov_)"
     }
-    response = requests.get(url,
-                            headers=request_headers,
-                            allow_redirects=False)
-    if response.status_code == 404:
+    response = requests.get(
+        url,
+        headers=request_headers,
+        allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    api_data = response.json().get("data")
-    return api_data.get("subscribers")
+    dic = response.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return response.json()['data']['subscribers']
