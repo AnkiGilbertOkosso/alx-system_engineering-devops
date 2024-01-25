d61bf50c2760606fd816a76088090f6684dcb0e3
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-01-25 20:02:28 +0100

    updates

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Fix problem of high traffic of requests
 
-exec {'rep':
+exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
 }
 
-exec {'res':
+exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
